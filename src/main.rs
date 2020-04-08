6fa14433c6466064822e3dacfc8a85e00cf40b39
Scott Jones <scottdj92@gmail.com>
2020-04-07 22:57:56 -0400

    small refactors

--- src/main.rs
+++ src/main.rs
@@ -7,41 +7,10 @@ use amethyst::{
     },
     utils::application_root_dir,
 };
-use amethyst::winit::*;
 
-struct GameState;
+mod wizard;
 
-impl SimpleState for GameState {
-    fn on_start(&mut self, _: StateData<'_, GameData<'_, '_>>) {
-        println!("Hello world");
-    }
-
-    fn on_stop(&mut self, _data: StateData<'_, GameData<'_, '_>>) {
-        println!("Goodbye world");
-    }
-
-    // fn update(&mut self, _: &mut StateData<'_, GameData<'_, '_>>) -> SimpleTrans {
-    //     println!("hello from amethyst");
-    //     Trans::Quit
-    // }
-
-    fn handle_event(&mut self, _data: StateData<'_, GameData<'_, '_>>, event: StateEvent) -> SimpleTrans {
-        if let StateEvent::Window(event) = &event {
-            match event {
-                Event::WindowEvent { event , .. } => match event {
-                    WindowEvent::KeyboardInput {
-                        input: KeyboardInput { virtual_keycode: Some(VirtualKeyCode::Escape), .. }, ..
-                    } |
-                    WindowEvent::CloseRequested => Trans::Quit,
-                    _ => Trans::None
-                },
-                _ => Trans::None
-            }
-        } else {
-            Trans::None
-        }
-    }
-}
+use crate::wizard::WizardState;
 
 fn main() -> amethyst::Result<()> {
     // set up loggers and load application assets
@@ -50,17 +19,16 @@ fn main() -> amethyst::Result<()> {
     let display_config_path = app_root.join("config").join("display.ron");
 
     // start renderer
-    let game_data = GameDataBuilder::default()
-        .with_bundle(
-            RenderingBundle::<DefaultBackend>::new()
-                .with_plugin(
-                    RenderToWindow::from_config_path(display_config_path)?
-                        .with_clear([0.0, 0.0, 0.0, 1.0]),
-                )
+    let game_data = GameDataBuilder::default().with_bundle(
+        RenderingBundle::<DefaultBackend>::new()
+            .with_plugin(
+                RenderToWindow::from_config_path(display_config_path)?
+                    .with_clear([0.0, 0.0, 0.0, 1.0]),
+            )
             .with_plugin(RenderFlat2D::default()),
-        )?;
+    )?;
 
-    let mut game = Application::new(app_root, GameState, game_data)?;
+    let mut game = Application::new(app_root, WizardState, game_data)?;
     game.run();
 
     Ok(())
---
+++ src/wizard.rs
@@ -0,0 +1,55 @@
+use amethyst::{
+    prelude::*,
+    core::transform::Transform,
+    renderer::{Camera}
+};
+
+use amethyst::winit::*;
+
+pub struct WizardState;
+pub const ARENA_HEIGHT: f32 = 100.0;
+pub const ARENA_WIDTH: f32 = 100.0;
+
+impl SimpleState for WizardState {
+    fn on_start(&mut self, _data: StateData<'_, GameData<'_, '_>>) {
+        println!("Hello world");
+        initialize_camera(_data.world);
+    }
+
+    fn on_stop(&mut self, _data: StateData<'_, GameData<'_, '_>>) {
+        println!("Goodbye world");
+    }
+
+    // fn update(&mut self, _: &mut StateData<'_, GameData<'_, '_>>) -> SimpleTrans {
+    //     println!("hello from amethyst");
+    //     Trans::Quit
+    // }
+
+    fn handle_event(&mut self, _data: StateData<'_, GameData<'_, '_>>, event: StateEvent) -> SimpleTrans {
+        if let StateEvent::Window(event) = &event {
+            match event {
+                Event::WindowEvent { event , .. } => match event {
+                    WindowEvent::KeyboardInput {
+                        input: KeyboardInput { virtual_keycode: Some(VirtualKeyCode::Escape), .. }, ..
+                    } |
+                    WindowEvent::CloseRequested => Trans::Quit,
+                    _ => Trans::None
+                },
+                _ => Trans::None
+            }
+        } else {
+            Trans::None
+        }
+    }
+}
+
+fn initialize_camera(world: &mut World) {
+    let mut transform = Transform::default();
+    transform.set_translation_xyz(ARENA_WIDTH * 0.5, ARENA_HEIGHT * 0.5, 1.0);
+
+    world
+        .create_entity()
+        .with(Camera::standard_2d(ARENA_WIDTH, ARENA_HEIGHT))
+        .with(transform)
+        .build();
+}
